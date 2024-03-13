95dfcbb9fa5de82bbcd1230bce013b619544a209
corvus <vip.egor.voronov.2016@mail.ru>
2024-03-13 13:39:54 +0300

    fix(modal): add local state to ticker modal

--- src/widgets/TickerModal/ui/TickerModal.tsx
+++ src/widgets/TickerModal/ui/TickerModal.tsx
@@ -1,16 +1,18 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Modal } from "@/shared/modal";
 import { useTickerModalStore } from "@/widgets";
 
 export const TickerModal: FC = () => {
   const store = useTickerModalStore;
+  const [localShowModal, setLocalShowModal] = useState(store.showModal);
 
   const handleClose = () => {
     store.closeModal();
+    setLocalShowModal(false);
   };
 
   return (
-    <Modal showModal={store.showModal} onClose={handleClose}>
+    <Modal showModal={localShowModal} onClose={handleClose}>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
       quidem.
     </Modal>
