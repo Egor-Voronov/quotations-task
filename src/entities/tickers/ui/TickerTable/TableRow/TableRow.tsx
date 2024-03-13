7eaa6fc98630303f3c478d9d3e5e19269de914d0
corvus <vip.egor.voronov.2016@mail.ru>
2024-03-13 07:57:47 +0300

    feat(tickers): add animaton to tickers

---
+++ src/entities/tickers/ui/TickerTable/TableRow/TableRow.tsx
@@ -0,0 +1,20 @@
+import type { FC } from "react";
+import type { ITicker } from "@/entities/tickers";
+import styles from "../TickerTable.module.css";
+
+const renderCell = <T extends string | number>(
+  value: T,
+  isChanged: boolean,
+) => <td className={isChanged ? styles.changed : ""}>{value}</td>;
+
+export const TableRow: FC<{ data: ITicker; isChanged: boolean }> = ({
+  data,
+  isChanged,
+}) => (
+  <tr>
+    {renderCell(data.symbol, isChanged)}
+    {renderCell(data.price, isChanged)}
+    {renderCell(data.bestBidPrice, isChanged)}
+    {renderCell(data.percentChange, isChanged)}
+  </tr>
+);
--- src/entities/tickers/ui/TickerTable/TickerTable.tsx
+++ src/entities/tickers/ui/TickerTable/TickerTable.tsx
@@ -1,8 +1,38 @@
+import { useState, useEffect, useRef } from "react";
 import type { FC } from "react";
 import type { TickerTableProps } from "./TickerTable.types.ts";
+import { TableRow } from "./TableRow";
 import styles from "./TickerTable.module.css";
 
 export const TickerTable: FC<TickerTableProps> = ({ data }) => {
+  const [changedCells, setChangedCells] = useState<Record<string, boolean>>({});
+  const prevData = useRef<typeof data>([]);
+
+  useEffect(() => {
+    const newChangedCells: Record<string, boolean> = {};
+    data.forEach((newItem, index) => {
+      const oldItem = prevData.current[index];
+      if (oldItem) {
+        if (
+          newItem.price !== oldItem.price ||
+          newItem.bestBidPrice !== oldItem.bestBidPrice ||
+          newItem.percentChange !== oldItem.percentChange
+        ) {
+          newChangedCells[newItem.tradeId] = true;
+        }
+      }
+    });
+
+    setChangedCells(newChangedCells);
+    prevData.current = data;
+
+    const timeout = setTimeout(() => {
+      setChangedCells({});
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [data]);
+
   return (
     <div>
       <table className={styles.table}>
@@ -16,12 +46,11 @@ export const TickerTable: FC<TickerTableProps> = ({ data }) => {
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item.tradeId}>
-              <td>{item.symbol}</td>
-              <td>{item.price}</td>
-              <td>{item.bestBidPrice}</td>
-              <td>{item.percentChange}</td>
-            </tr>
+            <TableRow
+              key={item.tradeId}
+              data={item}
+              isChanged={changedCells[item.tradeId]}
+            />
           ))}
         </tbody>
       </table>
